1040651db7ac6132814190cdf4503fc08c592048
serhii_dem <realsergeiii0@gmail.com>
2022-09-18 17:47:54 +0300

    simplelightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
- console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const listItemsGallery = createItemGalleryMarkup(galleryItems);
+
+function createItemGalleryMarkup(items) {
+  const galleryListMarkup = items.map(item => `<a class="gallery__item" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}" /></a>`);
+    return  galleryListMarkup.join('');
+      }
+  
+      galleryList.innerHTML = listItemsGallery;
+      galleryList.addEventListener('click', createModalShow);
+      function createModalShow (e) {
+e.preventDefault();
+new SimpleLightbox('.gallery a',
+{
+  captionsData:
+  'alt',
+  captionDelay:
+  250});
+  }
+
+console.log(galleryItems);
